8a75cd6d765a05d5a1e438fcdae9b05c1b912894
Владислав <143782996+WilyamVolos@users.noreply.github.com>
2024-07-23 22:59:52 +0300

    0723-webxml

--- out/artifacts/E_shop_war_exploded/WEB-INF/src/main/java/ru/academytop/eshop/config/ConfigBean.java
+++ out/artifacts/E_shop_war_exploded/WEB-INF/src/main/java/ru/academytop/eshop/config/ConfigBean.java
@@ -3,11 +3,13 @@ package ru.academytop.eshop.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+// Аннотация @Configuration указывает, что этот класс является конфигурационным классом Spring
 @Configuration
 public class ConfigBean {
+    // Аннотация @Bean указывает, что метод создает bean, который будет управляться Spring
     @Bean
     public ModelMapper modelMapper() {
+        // Создание и возврат экземпляра ModelMapper, который будет использоваться для маппинга объектов
         return new ModelMapper();
     }
 }
